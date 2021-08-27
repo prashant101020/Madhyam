554352eeb052cdc3c853a0144f17ef5f3d5fb35f
prashant101020 <pyss0206@gmail.com>
2021-08-27 15:48:11 +0530

    first commit

---
+++ react-blog/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./header.css"
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="headerTitles">
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTItleLg">Madhyam</span>
+            </div>
+            <img className="headerImg" src="https://pix10.agoda.net/hotelImages/568255/-1/041a32e55dffcb63d5fd44bd48e54d27.jpg?s=1024x768" alt="" />
+        </div>
+    )
+}
